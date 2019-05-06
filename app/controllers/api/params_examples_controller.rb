8bebe6c797a4a895fdbd3726e5ec9aa7c33dc1c5
robocop019 <chrisbowen@comcast.net>
2019-05-06 00:02:29 -0500

    segment/body params

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -2,6 +2,20 @@ class Api::ParamsExamplesController < ApplicationController
   def query_params
     @message = params[:my_message]
     @message_2 = params[:message_2]
+    
     render 'query_params.json.jbuilder'
+  end
+
+  def segment_params_action
+    @message = params[:wildcard]
+
+    render 'segment_params.json.jbuilder'
+  end
+
+  def body_params
+    input_value = params[:secret_info]
+    @message = "The secret message is #{input_value}"
+
+    render 'body_params.json.jbuilder'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/query_params' => 'params_examples#query_params'
+    get '/segment_params/:wildcard' => 'params_examples#segment_params_action'
+    post '/body_params' => 'params_examples#body_params'
   end
 end
